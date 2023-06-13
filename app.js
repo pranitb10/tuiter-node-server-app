fb012f163d0715edbbea0265d2deeed59de0516e
Pranit Brahmbhatt <pranit@esamudaay.com>
2023-06-12 20:45:16 -0400

    node server app created and pushed

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import session from "express-session";
+import AuthController from "./users/auth-controller.js";
+import cors from 'cors'
+const app = express();
+
+app.use(
+    session({
+      secret: "any string",
+      resave: false,
+      saveUninitialized: true,
+    })
+   );
+   
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+    })
+   );
+   
+app.use(express.json());
+const port = process.env.PORT || 4000;
+TuitsController(app);
+HelloController(app);
+UserController(app);
+AuthController(app);
+
+app.listen(port);
